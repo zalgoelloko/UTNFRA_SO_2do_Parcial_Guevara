1303ecc82d1712102b07d11d74e4504a94ad1d77
zalgoelloko <niko_geniall@hotmail.com>
2024-11-13 01:21:27 +0000

    Subiendo Punto_B.sh

---
+++ RTA_Examen_20241109/Punto_B.sh
@@ -0,0 +1,11 @@
+"Punto B
+El punto B realize: 
+-la creacion  de los grupos. 
+-Creación de Usuarios, Asignando como clave la misma que tiene el
+usuario que se pasa con el 1er parámetro.
+Mediante un script el cual toma como entrada un archivo que contiene una lista de usuarios, grupos y directorios y crea los usuarios como parametros
+me pocisiones en usr/local/bim
+para crear el archivo ejecutrable Guevara_AltaUser-Groups.sh
+para luego darle permisos para poder ejecutarlo sudo chmod +x Guevara_AltaUser-Groups.sh
+y despues lo ejecute con  sudo /usr/local/bin/Guevara_AltaUser-Groups.sh guevara /home/guevara/UTNFRA_SO_2do_Parcial_Guevara/202406/bash_script/Lista_Usuarios.txt"
+
